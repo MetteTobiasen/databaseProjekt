fc1876911e1e0b374aaeba60709224a9a958395d
Emil Johannes Lundqvist <emlunde@gmail.com>
2019-05-02 15:12:44 +0200

    got connection - created a user

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import data.dal.IUserDAO;
+import data.dal.UserDAO;
+import data.dto.UserDTO;
+
+public class Main {
+
+    public static void main(String[] args) throws IUserDAO.DALException {
+        UserDAO userDAO = new UserDAO();
+        UserDTO user = new UserDTO(23,"Hejsa");
+
+        userDAO.createUser(user);
+
+//        System.out.println(user.toString());
+    }
+}
