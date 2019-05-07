3ee35723b677e17273f5fec0efd716c1439c7b07
MetteTobiasen <Tobiasen_1@hotmail.com>
2019-05-07 22:11:57 +0200

    oprettet RoleDAO

---
+++ src/main/java/data/dal/RoleDAO.java
@@ -0,0 +1,17 @@
+package data.dal;
+
+import data.dto.RoleDTO;
+
+import java.io.Serializable;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoleDAO implements Serializable{
+    private final String url = "jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s185131?";
+    private final String userName= "user=s185131";
+    private final String pass = "password=f641omiIhm3Ly1oQR5khj";
+
+
+
+}
